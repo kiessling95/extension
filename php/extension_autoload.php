8747d8ad022d03d7e3320ad4398e60884123896a
german kiessling <gkiessling_95@hotmail.com>
2019-09-03 12:29:36 -0300

    carga nuevo proyecto extension

---
+++ php/extension_autoload.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Esta clase fue y será generada automáticamente. NO EDITAR A MANO.
+ * @ignore
+ */
+class extension_autoload 
+{
+	static function existe_clase($nombre)
+	{
+		return isset(self::$clases[$nombre]);
+	}
+
+	static function cargar($nombre)
+	{
+		if (self::existe_clase($nombre)) { 
+			 require_once(dirname(__FILE__) .'/'. self::$clases[$nombre]); 
+		}
+	}
+
+	static protected $clases = array(
+        'abm_ci' => 'extension_toba/componentes/abm_ci.php',
+		'extension_ci' => 'extension_toba/componentes/extension_ci.php',
+		'extension_cn' => 'extension_toba/componentes/extension_cn.php',
+		'extension_datos_relacion' => 'extension_toba/componentes/extension_datos_relacion.php',
+		'extension_datos_tabla' => 'extension_toba/componentes/extension_datos_tabla.php',
+		'extension_ei_arbol' => 'extension_toba/componentes/extension_ei_arbol.php',
+		'extension_ei_archivos' => 'extension_toba/componentes/extension_ei_archivos.php',
+		'extension_ei_calendario' => 'extension_toba/componentes/extension_ei_calendario.php',
+		'extension_ei_codigo' => 'extension_toba/componentes/extension_ei_codigo.php',
+		'extension_ei_cuadro' => 'extension_toba/componentes/extension_ei_cuadro.php',
+		'extension_ei_esquema' => 'extension_toba/componentes/extension_ei_esquema.php',
+		'extension_ei_filtro' => 'extension_toba/componentes/extension_ei_filtro.php',
+		'extension_ei_firma' => 'extension_toba/componentes/extension_ei_firma.php',
+		'extension_ei_formulario' => 'extension_toba/componentes/extension_ei_formulario.php',
+		'extension_ei_formulario_ml' => 'extension_toba/componentes/extension_ei_formulario_ml.php',
+		'extension_ei_grafico' => 'extension_toba/componentes/extension_ei_grafico.php',
+		'extension_ei_mapa' => 'extension_toba/componentes/extension_ei_mapa.php',
+		'extension_servicio_web' => 'extension_toba/componentes/extension_servicio_web.php',
+		'extension_comando' => 'extension_toba/extension_comando.php',
+		'extension_modelo' => 'extension_toba/extension_modelo.php',
+	);
+}
+?>
---
+++ www/aplicacion.php
@@ -0,0 +1,30 @@
+<?php
+
+# Proyecto
+define('apex_pa_proyecto', 'extension');
+
+# Ejecuta con metadatos compilados
+#define('apex_pa_metadatos_compilados', 1);
+
+# Deshabilita el autologin
+#define("apex_pa_validacion_debug", 0);
+
+# Activa el logger en modo 'debug'. Para modo info pasar a '6'
+define('apex_pa_log_archivo', true);
+define('apex_pa_log_archivo_nivel', 7);
+
+//--------------------------------------------------------------------------
+//------ Invocacion del nucleo del toba ------------------------------------
+//--------------------------------------------------------------------------
+if (isset($_SERVER['TOBA_DIR'])) {
+	$dir = $_SERVER['TOBA_DIR'].'/php'; 
+	$separador = (substr(PHP_OS, 0, 3) == 'WIN') ? ';.;' : ':.:';
+	ini_set('include_path', ini_get('include_path'). $separador . $dir);
+	require_once('nucleo/toba_nucleo.php');
+	toba_nucleo::instancia()->acceso_web();	
+} else {
+	die("Es necesario definir la variable 'TOBA_DIR' en el archivo de configuracion de apache
+			(Utilize la directiva 'SetEnv')");
+}
+//--------------------------------------------------------------------------
+?>
