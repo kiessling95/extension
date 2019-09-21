9c1adcebb54ab8ecf8cf512d825bb3e9f76362cc
german <gkiessling95@gmail.com>
2019-09-21 15:18:12 -0300

    Query sql

---
+++ BD/query.sql
@@ -0,0 +1,11 @@
+ALTER TABLE pextension ADD COLUMN  eje_tematico character(20);
+
+ALTER TABLE pextension ADD COLUMN descripcion_situacion character(200)
+
+ALTER TABLE pextension ADD COLUMN caracterizacion_poblacion character(150)
+
+ALTER TABLE pextension ADD COLUMN localizacion_geo character(20)
+
+ALTER TABLE pextension ADD COLUMN antecedente_participacion character(200)
+
+ALTER TABLE pextension ADD COLUMN importancia_necesidad character(200)
