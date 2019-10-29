a05bed6ef5a744f5b3f6f992aa19043053ed8439
karen <karen.flores@est.fi.uncoma.edu.ar>
2019-10-29 12:16:16 -0300

    Pantalla Organizaciones

--- metadatos/componentes/toba_datos_relacion/dump_1001000192.sql
+++ metadatos/componentes/toba_datos_relacion/dump_1001000192.sql
@@ -111,6 +111,24 @@ INSERT INTO apex_objeto_dependencias (proyecto, dep_id, objeto_consumidor, objet
 	NULL, --inicializar
 	'2'  --orden
 );
+--- FIN Grupo de desarrollo 1001
+
+--- INICIO Grupo de desarrollo 1002
+INSERT INTO apex_objeto_dependencias (proyecto, dep_id, objeto_consumidor, objeto_proveedor, identificador, parametros_a, parametros_b, parametros_c, inicializar, orden) VALUES (
+	'extension', --proyecto
+	'1002000029', --dep_id
+	'1001000192', --objeto_consumidor
+	'1002000054', --objeto_proveedor
+	'organizaciones_participantes', --identificador
+	'1', --parametros_a
+	'1', --parametros_b
+	NULL, --parametros_c
+	NULL, --inicializar
+	NULL  --orden
+);
+--- FIN Grupo de desarrollo 1002
+
+--- INICIO Grupo de desarrollo 1001
 INSERT INTO apex_objeto_dependencias (proyecto, dep_id, objeto_consumidor, objeto_proveedor, identificador, parametros_a, parametros_b, parametros_c, inicializar, orden) VALUES (
 	'extension', --proyecto
 	'1001000169', --dep_id
@@ -161,6 +179,18 @@ INSERT INTO apex_objeto_dependencias (proyecto, dep_id, objeto_consumidor, objet
 	NULL, --parametros_c
 	NULL, --inicializar
 	'9'  --orden
+);
+INSERT INTO apex_objeto_dependencias (proyecto, dep_id, objeto_consumidor, objeto_proveedor, identificador, parametros_a, parametros_b, parametros_c, inicializar, orden) VALUES (
+	'extension', --proyecto
+	'1002000030', --dep_id
+	'1001000192', --objeto_consumidor
+	'1002000055', --objeto_proveedor
+	'tipo_organizacion', --identificador
+	'1', --parametros_a
+	'1', --parametros_b
+	NULL, --parametros_c
+	NULL, --inicializar
+	NULL  --orden
 );
 --- FIN Grupo de desarrollo 1002
 
