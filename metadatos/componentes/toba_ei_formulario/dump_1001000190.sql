8829c96786ae5bdbba735c4f208d97ac85e7c164
german <gkiessling95@gmail.com>
2019-12-06 12:15:42 -0300

    Prueba merge s en nombre codirector

--- metadatos/componentes/toba_ei_formulario/dump_1001000190.sql
+++ metadatos/componentes/toba_ei_formulario/dump_1001000190.sql
@@ -2065,7 +2065,7 @@ INSERT INTO apex_objeto_ei_formulario_ef (objeto_ei_formulario_fila, objeto_ei_f
 	'0', --obligatorio
 	'0', --oculto_relaja_obligatorio
 	'4', --orden
-	'Nombre:', --etiqueta
+	'Nombres:', --etiqueta
 	NULL, --etiqueta_estilo
 	NULL, --descripcion
 	NULL, --colapsado
