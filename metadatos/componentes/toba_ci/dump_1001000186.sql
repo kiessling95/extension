6320e348821ce221ca8cc6a31dec25eaafacd3bf
german <gkiessling_95@hotmail.com>
2020-01-28 11:34:55 -0300

    Elimino datos no necesarios

--- metadatos/componentes/toba_ci/dump_1001000186.sql
+++ metadatos/componentes/toba_ci/dump_1001000186.sql
@@ -538,24 +538,6 @@ INSERT INTO apex_objeto_dependencias (proyecto, dep_id, objeto_consumidor, objet
 	NULL, --inicializar
 	NULL  --orden
 );
---- FIN Grupo de desarrollo 1002
-
---- INICIO Grupo de desarrollo 1001
-INSERT INTO apex_objeto_dependencias (proyecto, dep_id, objeto_consumidor, objeto_proveedor, identificador, parametros_a, parametros_b, parametros_c, inicializar, orden) VALUES (
-	'extension', --proyecto
-	'1001000153', --dep_id
-	'1001000186', --objeto_consumidor
-	'1001000191', --objeto_proveedor
-	'form_pext', --identificador
-	NULL, --parametros_a
-	NULL, --parametros_b
-	NULL, --parametros_c
-	NULL, --inicializar
-	NULL  --orden
-);
---- FIN Grupo de desarrollo 1001
-
---- INICIO Grupo de desarrollo 1002
 INSERT INTO apex_objeto_dependencias (proyecto, dep_id, objeto_consumidor, objeto_proveedor, identificador, parametros_a, parametros_b, parametros_c, inicializar, orden) VALUES (
 	'extension', --proyecto
 	'1002000022', --dep_id
@@ -906,13 +888,6 @@ INSERT INTO apex_objetos_pantalla (proyecto, pantalla, objeto_ci, orden, dep_id)
 	'extension', --proyecto
 	'1001000074', --pantalla
 	'1001000186', --objeto_ci
-	'0', --orden
-	'1001000153'  --dep_id
-);
-INSERT INTO apex_objetos_pantalla (proyecto, pantalla, objeto_ci, orden, dep_id) VALUES (
-	'extension', --proyecto
-	'1001000074', --pantalla
-	'1001000186', --objeto_ci
 	'3', --orden
 	'1001000171'  --dep_id
 );
@@ -929,13 +904,6 @@ INSERT INTO apex_objetos_pantalla (proyecto, pantalla, objeto_ci, orden, dep_id)
 	'1001000186', --objeto_ci
 	'1', --orden
 	'1001000214'  --dep_id
-);
-INSERT INTO apex_objetos_pantalla (proyecto, pantalla, objeto_ci, orden, dep_id) VALUES (
-	'extension', --proyecto
-	'1001000075', --pantalla
-	'1001000186', --objeto_ci
-	'0', --orden
-	'1001000153'  --dep_id
 );
 INSERT INTO apex_objetos_pantalla (proyecto, pantalla, objeto_ci, orden, dep_id) VALUES (
 	'extension', --proyecto
@@ -978,13 +946,6 @@ INSERT INTO apex_objetos_pantalla (proyecto, pantalla, objeto_ci, orden, dep_id)
 	'1001000186', --objeto_ci
 	'0', --orden
 	'1001000152'  --dep_id
-);
-INSERT INTO apex_objetos_pantalla (proyecto, pantalla, objeto_ci, orden, dep_id) VALUES (
-	'extension', --proyecto
-	'1001000087', --pantalla
-	'1001000186', --objeto_ci
-	'0', --orden
-	'1001000153'  --dep_id
 );
 INSERT INTO apex_objetos_pantalla (proyecto, pantalla, objeto_ci, orden, dep_id) VALUES (
 	'extension', --proyecto
