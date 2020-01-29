742e67a2fa90b4377d398e9b6526b94404970429
german <gkiessling_95@hotmail.com>
2020-01-29 12:02:27 -0300

    metadata presupuesto

--- metadatos/componentes/toba_ei_formulario/dump_1002000047.sql
+++ metadatos/componentes/toba_ei_formulario/dump_1002000047.sql
@@ -240,7 +240,7 @@ INSERT INTO apex_objeto_ei_formulario_ef (objeto_ei_formulario_fila, objeto_ei_f
 	'0', --obligatorio
 	'0', --oculto_relaja_obligatorio
 	'2', --orden
-	'Monto Maximo Rubro', --etiqueta
+	'Monto restante del Rubro', --etiqueta
 	NULL, --etiqueta_estilo
 	NULL, --descripcion
 	'0', --colapsado
