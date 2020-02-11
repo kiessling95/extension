931df1456713b4614dc9bab674c613827ce0d778
german <gkiessling_95@hotmail.com>
2020-02-11 11:10:26 -0300

    Agrego Claustro y modifico opc funcion_p
    Otros cambios

--- metadatos/componentes/toba_ei_cuadro/dump_1001000193.sql
+++ metadatos/componentes/toba_ei_cuadro/dump_1001000193.sql
@@ -135,7 +135,7 @@ INSERT INTO apex_objeto_ei_cuadro_columna (objeto_cuadro_proyecto, objeto_cuadro
 	'1001000193', --objeto_cuadro
 	'1001000470', --objeto_cuadro_col
 	'funcion_p', --clave
-	'5', --orden
+	'6', --orden
 	'Función en el proyecto', --titulo
 	'ei-cuadro-col-tit', --estilo_titulo
 	'4', --estilo
@@ -166,7 +166,7 @@ INSERT INTO apex_objeto_ei_cuadro_columna (objeto_cuadro_proyecto, objeto_cuadro
 	'1001000193', --objeto_cuadro
 	'1001000471', --objeto_cuadro_col
 	'carga_horaria', --clave
-	'8', --orden
+	'9', --orden
 	'Carga horaria', --titulo
 	'ei-cuadro-col-tit', --estilo_titulo
 	'0', --estilo
@@ -197,7 +197,7 @@ INSERT INTO apex_objeto_ei_cuadro_columna (objeto_cuadro_proyecto, objeto_cuadro
 	'1001000193', --objeto_cuadro
 	'1001000472', --objeto_cuadro_col
 	'hasta', --clave
-	'7', --orden
+	'8', --orden
 	'Fecha Fin en  Proyecto', --titulo
 	'ei-cuadro-col-tit', --estilo_titulo
 	'4', --estilo
@@ -228,7 +228,7 @@ INSERT INTO apex_objeto_ei_cuadro_columna (objeto_cuadro_proyecto, objeto_cuadro
 	'1001000193', --objeto_cuadro
 	'1001000474', --objeto_cuadro_col
 	'nombre', --clave
-	'1', --orden
+	'2', --orden
 	'Nombre y Apellido', --titulo
 	'ei-cuadro-col-tit', --estilo_titulo
 	'4', --estilo
@@ -259,7 +259,7 @@ INSERT INTO apex_objeto_ei_cuadro_columna (objeto_cuadro_proyecto, objeto_cuadro
 	'1001000193', --objeto_cuadro
 	'1001000475', --objeto_cuadro_col
 	'tipo_docum', --clave
-	'2', --orden
+	'3', --orden
 	'Tipo Doc', --titulo
 	'ei-cuadro-col-tit', --estilo_titulo
 	'4', --estilo
@@ -290,7 +290,7 @@ INSERT INTO apex_objeto_ei_cuadro_columna (objeto_cuadro_proyecto, objeto_cuadro
 	'1001000193', --objeto_cuadro
 	'1001000476', --objeto_cuadro_col
 	'nro_docum', --clave
-	'3', --orden
+	'4', --orden
 	'Documento', --titulo
 	'ei-cuadro-col-tit', --estilo_titulo
 	'4', --estilo
@@ -321,7 +321,7 @@ INSERT INTO apex_objeto_ei_cuadro_columna (objeto_cuadro_proyecto, objeto_cuadro
 	'1001000193', --objeto_cuadro
 	'1001000477', --objeto_cuadro_col
 	'tipo_sexo', --clave
-	'4', --orden
+	'5', --orden
 	'Genero', --titulo
 	'ei-cuadro-col-tit', --estilo_titulo
 	'4', --estilo
@@ -352,7 +352,7 @@ INSERT INTO apex_objeto_ei_cuadro_columna (objeto_cuadro_proyecto, objeto_cuadro
 	'1001000193', --objeto_cuadro
 	'1001000479', --objeto_cuadro_col
 	'desde', --clave
-	'6', --orden
+	'7', --orden
 	'Fecha Inicio en  Proyecto', --titulo
 	'ei-cuadro-col-tit', --estilo_titulo
 	'4', --estilo
@@ -383,7 +383,7 @@ INSERT INTO apex_objeto_ei_cuadro_columna (objeto_cuadro_proyecto, objeto_cuadro
 	'1001000193', --objeto_cuadro
 	'1001000526', --objeto_cuadro_col
 	'telefono', --clave
-	'10', --orden
+	'11', --orden
 	'Teléfono de contacto', --titulo
 	'ei-cuadro-col-tit', --estilo_titulo
 	'4', --estilo
@@ -414,7 +414,7 @@ INSERT INTO apex_objeto_ei_cuadro_columna (objeto_cuadro_proyecto, objeto_cuadro
 	'1001000193', --objeto_cuadro
 	'1001000527', --objeto_cuadro_col
 	'mail', --clave
-	'11', --orden
+	'12', --orden
 	'Email de contacto', --titulo
 	'ei-cuadro-col-tit', --estilo_titulo
 	'4', --estilo
@@ -445,8 +445,39 @@ INSERT INTO apex_objeto_ei_cuadro_columna (objeto_cuadro_proyecto, objeto_cuadro
 	'1001000193', --objeto_cuadro
 	'1001000541', --objeto_cuadro_col
 	'ad_honorem', --clave
-	'9', --orden
+	'10', --orden
 	'Ad honorem', --titulo
+	'ei-cuadro-col-tit', --estilo_titulo
+	'4', --estilo
+	NULL, --ancho
+	'1', --formateo
+	NULL, --vinculo_indice
+	NULL, --no_ordenar
+	NULL, --mostrar_xls
+	NULL, --mostrar_pdf
+	NULL, --pdf_propiedades
+	NULL, --desabilitado
+	NULL, --total
+	NULL, --total_cc
+	NULL, --usar_vinculo
+	NULL, --vinculo_carpeta
+	NULL, --vinculo_item
+	NULL, --vinculo_popup
+	NULL, --vinculo_popup_param
+	NULL, --vinculo_target
+	NULL, --vinculo_celda
+	NULL, --vinculo_servicio
+	'0', --permitir_html
+	NULL, --grupo
+	NULL  --evento_asociado
+);
+INSERT INTO apex_objeto_ei_cuadro_columna (objeto_cuadro_proyecto, objeto_cuadro, objeto_cuadro_col, clave, orden, titulo, estilo_titulo, estilo, ancho, formateo, vinculo_indice, no_ordenar, mostrar_xls, mostrar_pdf, pdf_propiedades, desabilitado, total, total_cc, usar_vinculo, vinculo_carpeta, vinculo_item, vinculo_popup, vinculo_popup_param, vinculo_target, vinculo_celda, vinculo_servicio, permitir_html, grupo, evento_asociado) VALUES (
+	'extension', --objeto_cuadro_proyecto
+	'1001000193', --objeto_cuadro
+	'1001000555', --objeto_cuadro_col
+	'tipo', --clave
+	'1', --orden
+	'Claustro', --titulo
 	'ei-cuadro-col-tit', --estilo_titulo
 	'4', --estilo
 	NULL, --ancho
--- php/datos/dt_funcion_extension.php
+++ php/datos/dt_funcion_extension.php
@@ -11,16 +11,23 @@ class dt_funcion_extension extends extension_datos_tabla
 	}
         function get_funcion_otros()
 	{
-		$sql = "SELECT t_f.id_extension, t_f.descripcion "
-                        . "FROM funcion_extension as t_f "
-                        . "LEFT OUTER JOIN integrante_externo_pe as t_e ON (t_e.funcion_p = t_f.id_extension) "
-                        . "WHERE t_f.id_extension='I' OR t_f.id_extension='AS' "
-                        . "OR t_f.id_extension='ND' "
-                        . "OR t_f.id_extension='C' OR t_f.id_extension='EST' "
-                        . "OR t_f.id_extension='G' OR t_f.id_extension='CE' "
-                        . "ORDER BY t_f.descripcion";
+		$sql = "SELECT id_extension, descripcion "
+                        . "FROM funcion_extension "
+                        . "WHERE id_extension='I' OR id_extension='AS' "
+                        . "OR id_extension='CD-Co' OR id_extension='CE' "
+                        . "ORDER BY descripcion";
 		return toba::db('extension')->consultar($sql);
 	}
+        
+        function get_claustro()
+	{
+		$sql = "SELECT id_extension, descripcion "
+                        . "FROM funcion_extension "
+                        . "WHERE id_extension='G' OR id_extension='EST' OR id_extension='ND' OR id_extension='I' OR id_extension='EXT'"
+                        . "ORDER BY descripcion";
+		return toba::db('extension')->consultar($sql);
+	}
+
         
 }
 ?>
