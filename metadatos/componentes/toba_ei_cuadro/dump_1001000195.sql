14d1748eb545d927345fe9c7e706dd1755dcd386
german <gkiessling_95@hotmail.com>
2020-03-10 07:55:13 -0300

    ajuste cuadro integrantes 100%

--- metadatos/componentes/toba_ei_cuadro/dump_1001000195.sql
+++ metadatos/componentes/toba_ei_cuadro/dump_1001000195.sql
@@ -54,7 +54,7 @@ INSERT INTO apex_objeto_cuadro (objeto_cuadro_proyecto, objeto_cuadro, titulo, s
 	NULL, --columna_descripcion
 	'0', --clave_dbr
 	NULL, --archivos_callbacks
-	NULL, --ancho
+	'100%', --ancho
 	'0', --ordenar
 	'0', --paginar
 	NULL, --tamano_pagina
