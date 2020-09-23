8d3273ccb0b29d6bdc5e830173d5dcda298e10bc
german <gkiessling_95@hotmail.com>
2020-09-23 10:48:30 -0300

    cambios solicitud
    diferencio entre proyecto y integrante

--- php/ci/form_solicitud_ocultar_mostrar.php
+++ php/ci/form_solicitud_ocultar_mostrar.php
@@ -1,12 +1,11 @@
 <?php
 
-    class form_solicitud_ocultar_mostrar extends toba_ei_formulario
-    {
-        function extender_objeto_js()
-	{
-		$id_js = toba::escaper()->escapeJs($this->objeto_js);
-                
-                echo "
+class form_solicitud_ocultar_mostrar extends toba_ei_formulario {
+
+    function extender_objeto_js() {
+        $id_js = toba::escaper()->escapeJs($this->objeto_js);
+
+        echo "
                     
                 {$id_js}.evt__tipo_solicitud__procesar = function(es_inicial) 
                 {
@@ -17,15 +16,33 @@
                         case 'INTEGRANTE':
                             this.ef('cambio_integrante').mostrar();
                             this.ef('estado_solicitud_aux1').mostrar();
+                            this.ef('recibido').mostrar();
                             this.ef('estado_solicitud_aux2').ocultar();
+                            
+                            this.ef('barra1_aux').ocultar();
+                            this.ef('barra2_aux').ocultar();
                             break;
                         case 'PROYECTO':
                             this.ef('cambio_proyecto').mostrar();
                             this.ef('estado_solicitud_aux2').mostrar();
+                            this.ef('recibido').mostrar();
                             this.ef('estado_solicitud_aux1').ocultar();
+                            
+                            this.ef('barra1').ocultar();
+                            this.ef('barra2').ocultar();
+                            this.ef('barra2_aux').ocultar();
+  
                             break;
                         default:
+                            this.ef('estado_solicitud_aux1').ocultar();
+                            this.ef('estado_solicitud_aux2').ocultar();
+                            this.ef('barra1').ocultar();
+                            this.ef('barra2').ocultar();
+                            this.ef('barra1_aux').ocultar();
+                            this.ef('barra2_aux').ocultar();
+                            this.ef('recibido').ocultar();
                             break;
+                            
                     }
                 }
 		
@@ -34,17 +51,37 @@
 
                     if(this.ef('recibido').chequeado())
                     {
+                        switch (this.ef('tipo_solicitud').get_estado()) {
+                            case 'INTEGRANTE':
+                                this.ef('descrip_ua').mostrar();
+                                this.ef('estado_solicitud_aux1').mostrar();
+                                
+                                break;
+                            case 'PROYECTO':
+                                    this.ef('descrip_ua').ocultar();
+                                    this.ef('estado_solicitud_aux2').mostrar();
+                                break;
+                            default:
+                                break;
+                        }
                         this.ef('fecha_solicitud').mostrar();
                         this.ef('fecha_recepcion').mostrar();
-                        this.ef('descrip_ua').mostrar();
-                        switch (this.ef('cambio_proyecto').get_estado()) {
-                        }
-                    }
-                    else
-                    {
+                        
+                        
+
+                    }else{
                         this.ef('descrip_ua').ocultar();
                         this.ef('fecha_solicitud').ocultar();
                         this.ef('fecha_recepcion').ocultar();
+                        
+                        this.ef('nro_acta').ocultar();
+                        this.ef('fecha_fin_prorroga').ocultar();
+                        this.ef('obs_resolucion').ocultar();
+                        this.ef('id_estado').ocultar();
+                        this.ef('fecha_dictamen').ocultar();
+                        this.ef('estado_solicitud_aux1').ocultar();
+                        this.ef('estado_solicitud_aux2').ocultar();
+
                     }
                 }
                 
@@ -57,31 +94,16 @@
                                             this.ef('nro_acta').mostrar();
                                             this.ef('fecha_dictamen').mostrar();
                                             this.ef('obs_resolucion').mostrar();
+                                            this.ef('descrip_ua').mostrar();
+                                            
                                             this.ef('fecha_fin_prorroga').ocultar();
-
-                                            switch (this.ef('cambio_proyecto').get_estado()) {
-                                                    case 'BAJA':
-                                                        this.ef('id_estado').mostrar();
-                                                    break;
-
-                                                    case 'PRORROGA': 
-                                                            this.ef('fecha_fin_prorroga').mostrar();
-                                                            this.ef('id_estado').mostrar();
-                                                    break;
-
-                                                    case 'FINALIZACIÓN':
-                                                        this.ef('id_estado').mostrar();
-                                                    break;
-
-                                                    default:
-                                                        this.ef('id_estado').ocultar();
-                                                    break;
-                                            }
+                                            this.ef('id_estado').ocultar();
                                             break;
                                     case 'Rechazada':
                                             this.ef('nro_acta').mostrar(); 
                                             this.ef('fecha_dictamen').mostrar();
                                             this.ef('obs_resolucion').mostrar();
+                                            this.ef('descrip_ua').mostrar();
 
                                             this.ef('fecha_fin_prorroga').ocultar();
                                             this.ef('id_estado').ocultar();
@@ -101,25 +123,21 @@
                                             this.ef('fecha_dictamen').ocultar();
                                             break;
                                     default:
-                                            this.ef('recibido').ocultar();
-                                            this.ef('descrip_ua').ocultar();
-                                            this.ef('nro_acta').ocultar();
-                                            this.ef('fecha_fin_prorroga').ocultar();
-                                            this.ef('obs_resolucion').ocultar();
-                                            this.ef('id_estado').ocultar();
-                                            this.ef('fecha_dictamen').ocultar();
+                                            
+                                            
                                            break;
                     }
                 }
                 
                 {$id_js}.evt__estado_solicitud_aux2__procesar = function(es_inicial) 
                 {
-                    switch (this.ef('estado_solicitud_aux1').get_estado()) {
+                    switch (this.ef('estado_solicitud_aux2').get_estado()) {
                                     case 'Aceptada':
                                             this.ef('nro_acta').mostrar();
                                             this.ef('fecha_dictamen').mostrar();
                                             this.ef('obs_resolucion').mostrar();
                                             this.ef('fecha_fin_prorroga').ocultar();
+                                            this.ef('descrip_ua').ocultar();
 
                                             switch (this.ef('cambio_proyecto').get_estado()) {
                                                     case 'BAJA':
@@ -131,7 +149,7 @@
                                                             this.ef('id_estado').mostrar();
                                                     break;
 
-                                                    case 'FINALIZACIÓN':
+                                                    case 'FINALIZACION':
                                                         this.ef('id_estado').mostrar();
                                                     break;
 
@@ -147,6 +165,7 @@
 
                                             this.ef('fecha_fin_prorroga').ocultar();
                                             this.ef('id_estado').ocultar();
+
                                             break;
                                     case 'Enviada':
                                             this.ef('nro_acta').ocultar();
@@ -154,6 +173,8 @@
                                             this.ef('obs_resolucion').ocultar();
                                             this.ef('id_estado').ocultar();
                                             this.ef('fecha_dictamen').ocultar();
+                                            this.ef('descrip_ua').ocultar();
+                                            
                                             break;
                                     case 'Recibida':
                                             this.ef('nro_acta').ocultar();
@@ -161,23 +182,18 @@
                                             this.ef('obs_resolucion').ocultar();
                                             this.ef('id_estado').ocultar();
                                             this.ef('fecha_dictamen').ocultar();
+                                            this.ef('descrip_ua').ocultar();
                                             break;
                                     default:
-                                            this.ef('recibido').ocultar();
-                                            this.ef('descrip_ua').ocultar();
-                                            this.ef('nro_acta').ocultar();
-                                            this.ef('fecha_fin_prorroga').ocultar();
-                                            this.ef('obs_resolucion').ocultar();
-                                            this.ef('id_estado').ocultar();
-                                            this.ef('fecha_dictamen').ocultar();
+   
                                            break;
                     }
                 }
                 
                 
                 ";
-                
-        }
     }
+
+}
 
 ?>
