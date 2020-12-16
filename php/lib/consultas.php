913dd76a6f21b47f98e8d0a725cdbf0ec4c4a65d
karen <karen.flores@est.fi.uncoma.edu.ar>
2020-12-16 10:56:12 -0300

    últimos cambios varios

--- php/lib/consultas.php
+++ php/lib/consultas.php
@@ -5,7 +5,7 @@ class consultas {
     static function get_observaciones_existentes($id_pext) {
         $schema_public = toba::db('extension')->get_schema(). '_auditoria';
         $sql = "SELECT id_pext, auditoria_fecha, auditoria_usuario, observacion_ua FROM $schema_public.logs_seguimiento_ua "
-                . "WHERE id_pext = $id_pext ";
+                . "WHERE id_pext = $id_pext AND observacion_ua <> ''";
 		return toba::db('extension')->consultar($sql);
     }
     
